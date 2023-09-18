b0af45c9d9553120afad8fd3a485fcb06783ec0d
ngunnnn <ngunnnn@gmail.com>
2023-09-18 14:10:38 +0900

    reduce noise

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import cv2
 
 # read video
 
-cap = cv2.VideoCapture("Coin2.mp4") #if open camera just index of camera
+cap = cv2.VideoCapture("Coin2.mp4")                     # if open camera just index of camera
 
 while(cap.read()) :
     ref, frame = cap.read()
+    roi = frame[:1080, 0:1920]                          # region of interest
 
-    cv2.imshow("Show", frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)      # convert to gray pic
+    gray_blur = cv2.GaussianBlur(gray, (15, 15), 0)     # reduce noise
+
+    # show
+    cv2.imshow("Show", gray_blur)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
