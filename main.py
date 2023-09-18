1ee23ae7d4b31a79c56aba9be9fb982aedf684e0
ngunnnn <ngunnnn@gmail.com>
2023-09-18 14:25:22 +0900

    adaptive threshold

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ while(cap.read()) :
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)      # convert to gray pic
     gray_blur = cv2.GaussianBlur(gray, (15, 15), 0)     # reduce noise
 
+    thresh = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 1)
+                                                        #adaptive threshold
+
     # show
-    cv2.imshow("Show", gray_blur)
+    cv2.imshow("Show", thresh)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
